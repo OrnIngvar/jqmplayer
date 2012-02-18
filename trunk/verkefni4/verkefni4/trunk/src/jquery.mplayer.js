9c53705cfea9c741249f3d266c7f90f5e807ffbc
davidhalldor <davidhalldor@d58784cb-d099-e77e-8d7b-cb302dc4b424>
2012-02-18 14:20:35 +0000

    Búinn að setja upp verkefnið

---
+++ trunk/verkefni4/verkefni4/trunk/src/jquery.mplayer.js
@@ -0,0 +1,10 @@
+( function ( $ ) {
+    $.fn.musicPlayer = function( userOpt ){
+        // In this case, "this" refers to the
+        // collection of elements
+
+        return this.each( function( ){
+        // Here, "this" referes to the current elem
+        });
+    };
+}) ( jQuery );
